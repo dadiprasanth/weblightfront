c5c71a55e534fe481f8050c305e90472e915696e
dadiprasanth <dadiprasanth24@gmail.com>
2022-11-18 22:15:08 +0530

    added rate and cart option

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,17 +6,23 @@ export default function Card(props) {
     <div class="product">
             <div class="imagecontainer">
                 <div class="topicons">
-                    <span>75%</span>
-                    <i class="fa fa-eye"></i>
+                    <span>{data.rating.rate}/5</span>
+                    <span>Avl {data.rating.count}</span>
                 </div>
                 <div class="image">
                     <img src={data.image}/>
                 </div>
 
             </div>
+            <div class="hover">
+                <span>${data.price}</span>
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+                
+                </div>
             <div class="productname">
                 {data.title}
             </div>
+          
             </div>
   )
 }
