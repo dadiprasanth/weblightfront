b616da5b97a1773e65897b970f3f93fd99f5910d
dadiprasanth <dadiprasanth24@gmail.com>
2022-11-18 21:30:42 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import{useEffect, useState} from "react"
 function App() {
+  const[data,setdata]=useState("")
+  useEffect(()=>{
+    fetch("https://fakestoreapi.com/products" 
+    ).then(x=>x.json()).then(y=>setdata(y))
+    },[])
+
+  console.log(data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
