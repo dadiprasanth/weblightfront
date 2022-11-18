e33d98ac55f69475244ad96c53e0f76d12164f8a
dadiprasanth <dadiprasanth24@gmail.com>
2022-11-18 21:36:53 +0530

    added card component

---
+++ src/components/card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function card() {
+  return (
+    <div class="product">
+            <div class="imagecontainer">
+                <div class="topicons">
+                    <span>75%</span>
+                    <i class="fa fa-eye"></i>
+                </div>
+                <div class="image">
+                    <img src="acentChair.jpg"/>
+                </div>
+
+            </div>
+            <div class="productname">
+                green wood chair
+            </div>
+            </div>
+  )
+}
